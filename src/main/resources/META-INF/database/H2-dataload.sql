606a0ada57f10a801c5e2a05ec7ac489c2049145
Toshiaki Maki <makingx@gmail.com>
2014-02-12 00:55:45 +0900

    modified test data

--- src/main/resources/META-INF/database/H2-dataload.sql
+++ src/main/resources/META-INF/database/H2-dataload.sql
@@ -2,8 +2,8 @@ insert into role(role_name, version, created_date, created_by, last_modified_dat
 insert into role(role_name, version, created_date, created_by, last_modified_date, last_modified_by) values('EDITOR', 0, now(), 'admin', now(), 'admin');
 insert into user (username, email, enabled, first_name, last_name, locked, password, version, created_date, created_by, last_modified_date, last_modified_by) values('admin', 'admin@example.com', true, 'Admin', 'Admin', false, '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', 0, now(), 'admin', now(), 'admin');
 insert into user_role(username, role_id) select 'admin' as username, role_id from role;
-insert into link(url, link_name, version, created_date, created_by, last_modified_date, last_modified_by) values('Google', 'http://google.com', 0, now(), 'admin', now(), 'admin');
-insert into link(url, link_name, version, created_date, created_by, last_modified_date, last_modified_by) values('Twitter', 'http://twitter.com', 0, now(), 'admin', now(), 'admin');
+insert into link(url, link_name, version, created_date, created_by, last_modified_date, last_modified_by) values('http://google.com', 'Google', 0, now(), 'admin', now(), 'admin');
+insert into link(url, link_name, version, created_date, created_by, last_modified_date, last_modified_by) values('http://twitter.com', 'Twitter', 0, now(), 'admin', now(), 'admin');
 
 insert into entry (version, created_by, created_date, last_modified_by, last_modified_date, contents, format, published, title) values (0, 'admin', now(), 'admin', now(), '* aa\n* bb\n* cc', 'md', 1, 'This is a sample contents');
 insert into category (category_name, category_order, entry_id) values ('Contents', 1, 1);
