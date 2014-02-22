21c2b4ddee7f67b668cc7a54e059c6d976f7345f
Toshiaki Maki <makingx@gmail.com>
2014-02-22 22:12:58 +0900

    add report api #31

--- src/main/java/am/ik/categolj2/domain/repository/accesslog/AccessLogRepository.java
+++ src/main/java/am/ik/categolj2/domain/repository/accesslog/AccessLogRepository.java
@@ -16,7 +16,32 @@
 package am.ik.categolj2.domain.repository.accesslog;
 
 import am.ik.categolj2.domain.model.AccessLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AccessLogRepository extends JpaRepository<AccessLog, String> {
+    @Query("SELECT x FROM AccessLog x ORDER BY x.accessDate DESC")
+    Page<AccessLog> findPageOrderByAccessDateDesc(Pageable pageable);
+
+    @Query("SELECT NEW am.ik.categolj2.domain.repository.accesslog.AccessLogReportGroupByUri(x.uri, COUNT(x.uri))" +
+            " FROM AccessLog x GROUP BY x.uri HAVING x.uri LIKE :uri ORDER BY COUNT(x.uri) DESC")
+    List<AccessLogReportGroupByUri> findAllReportGroupByUriLike(@Param("uri") String uri);
+
+    @Query("SELECT NEW am.ik.categolj2.domain.repository.accesslog.AccessLogReportGroupByUri(x.uri, COUNT(x.uri))" +
+            " FROM AccessLog x GROUP BY x.uri ORDER BY COUNT(x.uri) DESC")
+    List<AccessLogReportGroupByUri> findAllReportGroupByUri();
+
+    @Query("SELECT NEW am.ik.categolj2.domain.repository.accesslog.AccessLogReportGroupByUriAndRemoteAddress(x.uri, x.remoteAddress, COUNT(x.remoteAddress))" +
+            " FROM AccessLog x GROUP BY x.uri, x.remoteAddress ORDER BY x.uri, COUNT(x.uri) DESC")
+    List<AccessLogReportGroupByUriAndRemoteAddress> findAllReportGroupByUriAndRemoteAddress();
+
+    @Query("DELETE FROM AccessLog x WHERE x.remoteAddress = :remoteAddress")
+    @Modifying
+    int deleteFromRemoteAddress(@Param("remoteAddress") String remoteAddress);
 }
