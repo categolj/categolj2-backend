9ee8c190c1906fa33b7ad4c8fec6c4e354b0febe
Toshiaki Maki <makingx@gmail.com>
2014-04-06 02:12:24 +0900

    support embedded-tomcat #54

---
+++ setenv.sh
@@ -0,0 +1,1 @@
+JAVA_OPTS="-Xms1g -Xmx1g -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -XX:+HeapDumpOnOutOfMemoryError"
