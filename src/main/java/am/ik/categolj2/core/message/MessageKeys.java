a07b5b4d39788b9a85cc988d5d76bbaea0073d2b
Toshiaki Maki <makingx@gmail.com>
2014-05-12 19:56:16 +0900

    added message keys #105

---
+++ src/main/java/am/ik/categolj2/core/message/MessageKeys.java
@@ -0,0 +1,62 @@
+package am.ik.categolj2.core.message;
+/**
+ * Message Id
+ */
+public class MessageKeys {
+    /** e.ct.fw.5001=Resource not found. */
+    public static final String E_CT_FW_5001 = "e.ct.fw.5001";
+    /** e.ct.fw.6001=Request method not supported. */
+    public static final String E_CT_FW_6001 = "e.ct.fw.6001";
+    /** e.ct.fw.6002=Specified representation format not supported. */
+    public static final String E_CT_FW_6002 = "e.ct.fw.6002";
+    /** e.ct.fw.6003=Specified media type in the request body not supported. */
+    public static final String E_CT_FW_6003 = "e.ct.fw.6003";
+    /** e.ct.fw.7001=Validation error occurred on item in the request body. */
+    public static final String E_CT_FW_7001 = "e.ct.fw.7001";
+    /** e.ct.fw.7002=Validation error occurred on item in the request parameters. */
+    public static final String E_CT_FW_7002 = "e.ct.fw.7002";
+    /** e.ct.fw.7003=Request body format error occurred. */
+    public static final String E_CT_FW_7003 = "e.ct.fw.7003";
+    /** e.ct.fw.7004=Unknown field exists in JSON. */
+    public static final String E_CT_FW_7004 = "e.ct.fw.7004";
+    /** e.ct.fw.7005=Type mismatch error occurred in JSON field. */
+    public static final String E_CT_FW_7005 = "e.ct.fw.7005";
+    /** e.ct.fw.7006=Type mismatch error occurred in request parameter or header or path variable. */
+    public static final String E_CT_FW_7006 = "e.ct.fw.7006";
+    /** e.ct.fw.8001=Business error occurred. */
+    public static final String E_CT_FW_8001 = "e.ct.fw.8001";
+    /** e.ct.fw.8002=Conflict with other processing occurred. */
+    public static final String E_CT_FW_8002 = "e.ct.fw.8002";
+    /** e.ct.us.8101=Th requested user is not found. [username={0}] */
+    public static final String E_CT_US_8101 = "e.ct.us.8101";
+    /** e.ct.us.8102=The given username (or email) is not found. [username={0}] */
+    public static final String E_CT_US_8102 = "e.ct.us.8102";
+    /** e.ct.us.8103=The given username is already used. [username={0}] */
+    public static final String E_CT_US_8103 = "e.ct.us.8103";
+    /** e.ct.us.8104=The given email is already used. [email={0}] */
+    public static final String E_CT_US_8104 = "e.ct.us.8104";
+    /** e.ct.us.8105=The given user does not exist. [username={0}] */
+    public static final String E_CT_US_8105 = "e.ct.us.8105";
+    /** e.ct.us.8106=At least one active admin must exist! */
+    public static final String E_CT_US_8106 = "e.ct.us.8106";
+    /** e.ct.us.8107=At least one unlocked admin must exist! */
+    public static final String E_CT_US_8107 = "e.ct.us.8107";
+    /** e.ct.us.8108=At least one enabled admin must exist! */
+    public static final String E_CT_US_8108 = "e.ct.us.8108";
+    /** e.ct.us.8109=The given role is invalid. [roleId={0}] */
+    public static final String E_CT_US_8109 = "e.ct.us.8109";
+    /** e.ct.en.8201=The requested entry is not found. [entryId={0}] */
+    public static final String E_CT_EN_8201 = "e.ct.en.8201";
+    /** e.ct.ln.8301=The requested link is not found. [url={0}] */
+    public static final String E_CT_LN_8301 = "e.ct.ln.8301";
+    /** e.ct.uf.8401=The requested file is not found. [fileId={0}] */
+    public static final String E_CT_UF_8401 = "e.ct.uf.8401";
+    /** e.ct.fw.9001=System error occurred. */
+    public static final String E_CT_FW_9001 = "e.ct.fw.9001";
+    /** e.ct.fw.9002=DataAccess error occurred. */
+    public static final String E_CT_FW_9002 = "e.ct.fw.9002";
+    /** e.ct.fw.9003=System error occurred. */
+    public static final String E_CT_FW_9003 = "e.ct.fw.9003";
+    /** e.ct.bo.9501=Search API execution failed */
+    public static final String E_CT_BO_9501 = "e.ct.bo.9501";
+}
