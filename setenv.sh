6f93edd22a0029fc28bccbabaf042da33af923bc
Toshiaki Maki <makingx@gmail.com>
2014-11-13 11:02:04 +0900

    delete unused shellscript

--- setenv.sh
+++
@@ -1,1 +0,0 @@
-JAVA_OPTS="-Xms1g -Xmx1g -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -XX:+HeapDumpOnOutOfMemoryError"
