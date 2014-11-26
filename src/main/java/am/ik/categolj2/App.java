22fb447729097c319512665752330765a25129d1
Toshiaki Maki <makingx@gmail.com>
2014-11-26 10:11:25 +0900

    prevent c90011 from H2 1.4

--- src/main/java/am/ik/categolj2/App.java
+++ src/main/java/am/ik/categolj2/App.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class App {
 
     public static void main(String[] args) {
+        if (System.getProperty("h2.implicitRelativePath") == null) {
+            // keep compatibility with H2 1.3
+            // prevent http://www.h2database.com/javadoc/org/h2/api/ErrorCode.html#c90011
+            System.setProperty("h2.implicitRelativePath", "true");
+        }
         SpringApplication.run(App.class, args);
     }
 }
