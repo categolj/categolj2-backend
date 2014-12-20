2b6d48d160cdd057aefd9924785b7a7ce7cc8a0b
Toshiaki Maki <makingx@gmail.com>
2014-12-20 13:43:57 +0900

    support @SpringBootApplication

--- src/main/java/am/ik/categolj2/App.java
+++ src/main/java/am/ik/categolj2/App.java
@@ -1,11 +1,9 @@
 package am.ik.categolj2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
