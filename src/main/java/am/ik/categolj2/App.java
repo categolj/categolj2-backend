3505f6edf436ce28c493ad0356686c05147f744a
Toshiaki Maki <makingx@gmail.com>
2014-12-20 20:07:42 +0900

    polished

--- src/main/java/am/ik/categolj2/App.java
+++ src/main/java/am/ik/categolj2/App.java
@@ -9,24 +9,18 @@ import java.util.stream.Stream;
 public class App {
 
     public static void main(String[] args) {
-        argsToSyetemProperty(args, "log.verbose", "log.sql", "log.sql.result");
+        argsToSystemProperty(args, "log.verbose", "log.sql", "log.sql.result");
         keepCompatibilityH2_13();
         SpringApplication.run(App.class, args);
     }
 
-    static void argsToSyetemProperty(String[] args, String... keys) {
-        Stream.of(keys).forEach(key -> {
-            Stream.of(args)
-                    .filter(x -> x.startsWith("--" + key + "="))
-                    .map(x -> x.substring(("--" + key + "=").length()))
-                    .map(String::trim)
-                    .findFirst()
-                    .map(x -> {
-                        System.out.println("match " + key + " ->" + x);
-                        return x;
-                    })
-                    .ifPresent(x -> System.setProperty(key, x));
-        });
+    static void argsToSystemProperty(String[] args, String... keys) {
+        Stream.of(keys).forEach(key -> Stream.of(args)
+                .filter(x -> x.startsWith("--" + key + "="))
+                .map(x -> x.substring(("--" + key + "=").length()))
+                .map(String::trim)
+                .findFirst()
+                .ifPresent(x -> System.setProperty(key, x)));
     }
 
     static void keepCompatibilityH2_13() {
