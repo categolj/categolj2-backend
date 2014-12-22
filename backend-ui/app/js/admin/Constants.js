9c8456800c00477a09c1d408da223307a21bb63f
Toshiaki Maki <makingx@gmail.com>
2014-12-23 04:01:34 +0900

    chagend url

--- backend-ui/app/js/admin/Constants.js
+++ backend-ui/app/js/admin/Constants.js
@@ -1,6 +1,6 @@
 define(function (require) {
-    var BACKEND_HOST = 'http://blog.ik.am/';
-    // var BACKEND_HOST = '';
+    // var BACKEND_HOST = 'http://blog.ik.am/';
+    var BACKEND_HOST = '';
     return {
         API_ROOT: BACKEND_HOST + 'api/v1',
         MANAGEMENT_ROOT: BACKEND_HOST + 'management'
