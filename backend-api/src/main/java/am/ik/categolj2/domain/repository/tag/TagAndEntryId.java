aec0cf833ea054ac266bf89865a8b81345a0e819
Toshiaki Maki <makingx@gmail.com>
2015-01-01 23:01:15 +0900

    polished

--- backend-api/src/main/java/am/ik/categolj2/domain/repository/tag/TagAndEntryId.java
+++ backend-api/src/main/java/am/ik/categolj2/domain/repository/tag/TagAndEntryId.java
@@ -2,10 +2,8 @@ package am.ik.categolj2.domain.repository.tag;
 
 import am.ik.categolj2.domain.model.Tag;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class TagAndEntryId {
     private final Tag tag;
     private final Integer entryId;
