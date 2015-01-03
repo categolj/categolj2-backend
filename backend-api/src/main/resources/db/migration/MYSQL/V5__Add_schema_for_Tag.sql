77f8b3fd98f852440989ee5b3435d998d9738488
Toshiaki Maki <makingx@gmail.com>
2015-01-03 19:19:55 +0900

    modified sql

--- backend-api/src/main/resources/db/migration/MYSQL/V5__Add_schema_for_Tag.sql
+++ backend-api/src/main/resources/db/migration/MYSQL/V5__Add_schema_for_Tag.sql
@@ -1,22 +1,14 @@
-CREATE TABLE entry_tags (
-  entry INTEGER      NOT NULL,
-  tags  VARCHAR(255) NOT NULL,
-  PRIMARY KEY (entry, tags)
-)
-  ENGINE =InnoDB;
-
 CREATE TABLE tag (
   tag_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (tag_name)
 )
   ENGINE =InnoDB;
 
-ALTER TABLE entry_tags
-ADD CONSTRAINT FK_a4axhndj52pjnbyd5dg66q0gu
-FOREIGN KEY (tags)
-REFERENCES tag (tag_name);
-
-ALTER TABLE entry_tags
-ADD CONSTRAINT FK_5h7a631jgq73p168bbhp4528p
-FOREIGN KEY (entry)
-REFERENCES entry (entry_id);
+CREATE TABLE entry_tags (
+  entry INTEGER      NOT NULL,
+  tags  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (entry, tags),
+  FOREIGN KEY (entry) REFERENCES entry (entry_id),
+  FOREIGN KEY (tags) REFERENCES tag (tag_name)
+)
+  ENGINE =InnoDB;
