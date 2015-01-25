7c0b4bdbb25834e77b2b8097a3b05e31f86d6b63
Toshiaki Maki <makingx@gmail.com>
2015-01-26 00:45:39 +0900

    ignore Crawler in saving accesslog

--- backend-api/src/main/java/am/ik/categolj2/domain/service/accesslog/AccessLogHelper.java
+++ backend-api/src/main/java/am/ik/categolj2/domain/service/accesslog/AccessLogHelper.java
@@ -2,6 +2,7 @@ package am.ik.categolj2.domain.service.accesslog;
 
 
 import am.ik.categolj2.domain.model.AccessLog;
+import is.tagomor.woothee.Classifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,13 @@ import javax.inject.Inject;
 public class AccessLogHelper {
     @Value("${accesslog.disabled:false}")
     boolean isAccessLogDisabled;
+    @Value("${accesslog.ignore.bot:true}")
+    boolean ignoreCrawler;
     @Inject
     AccessLogService accessLogService;
 
     public void writeIfAccessLogIsEnabled(AccessLog accessLog) {
-        if (!isAccessLogDisabled) {
+        if (!isAccessLogDisabled && !(ignoreCrawler && Classifier.isCrawler(accessLog.getUserAgent()))) {
             accessLogService.save(accessLog);
         }
     }
