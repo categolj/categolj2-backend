8461b13cc844389621e5e63ec2734b7e6ce89b08
Toshiaki Maki <makingx@gmail.com>
2015-03-10 14:34:25 +0900

    make config.js configurable by external resource

--- frontend-ui/app/js/config.js
+++ frontend-ui/app/js/config.js
@@ -1,8 +1,22 @@
 var FRONTEND_ROOT = 'http://blog.ik.am/';
-
-module.exports = {
+var defaults = {
     FRONTEND_ROOT: FRONTEND_ROOT,
     API_ROOT: 'api/v1',
     SEPARATOR: '::',
     BLOG_TITLE: 'BLOG.IK.AM'
+};
+
+var Config;
+if (!window.Config) {
+    Config = {};
+} else {
+    Config = window.Config;
 }
+
+for (var propName in defaults) {
+    if (!Config[propName]) {
+        Config[propName] = defaults[propName];
+    }
+}
+
+module.exports = Config;
